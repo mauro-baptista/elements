f810bbceaa6ad17c75a687ae996b1dc526fc1614
Mauro Baptista <mauro@carnou.com>
2022-12-27 12:10:36 -0300

    Fix merge attributes

--- resources/views/components/content/half.blade.php
+++ resources/views/components/content/half.blade.php
@@ -4,7 +4,7 @@
     <dt class="text-sm font-medium text-gray-500">
         {{ $label }}
     </dt>
-    <dd {{ $attributes->merge(['mt-1 text-sm text-gray-900']) }}>
+    <dd {{ $attributes->class(['mt-1 text-sm text-gray-900']) }}>
         {{ $slot }}
     </dd>
 </div>
