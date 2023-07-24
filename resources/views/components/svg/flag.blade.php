dabb56d7e1f39dd6e08233d568d880ae1ead5b38
Mauro Baptista <mauro@carnou.com>
2023-07-24 09:02:22 -0400

    fixed wrong method call

--- resources/views/components/svg/flag.blade.php
+++ resources/views/components/svg/flag.blade.php
@@ -1,3 +1,3 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" {{ $attributes->getAttribute('class') ?? 'h5 w-5' }} {{ $attributes->except('class') }}>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" {{ $attributes->getAttributes()['class'] ?? 'h5 w-5' }} {{ $attributes->except('class') }}>
     <path fill-rule="evenodd" d="M3 6a3 3 0 013-3h10a1 1 0 01.8 1.6L14.25 8l2.55 3.4A1 1 0 0116 13H6a1 1 0 00-1 1v3a1 1 0 11-2 0V6z" clip-rule="evenodd" />
 </svg>
