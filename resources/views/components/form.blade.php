1cbcaa0c0005a246de7b1226aaf151018867b180
Mauro Baptista <mauro@carnou.com>
2023-07-25 09:45:14 -0400

    Improved form loading state

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -47,7 +47,22 @@
                     <x-elements::message :on="$on" />
                 @endif
 
-                <x-elements::form.submit>{{ $submit }}</x-elements::form.submit>
+                <div wire:loading.remove>
+                    <x-elements::form.submit>
+                        {{ $submit }}
+                    </x-elements::form.submit>
+                </div>
+                <div wire:loading>
+                    <x-elements::button.default class="bg-main-200 text-white">
+                        <svg class="w-5 h-5 stroke-current" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: none; display: block; shape-rendering: auto;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+                            <circle cx="50" cy="50" fill="none" stroke-width="10" r="35" stroke-dasharray="164.93361431346415 56.97787143782138">
+                                <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1"/>
+                            </circle>
+                        </svg>
+
+                        <div class="ml-2">{{ $submit }}</div>
+                    </x-elements::button.default>
+                </div>
             </x-slot:footer>
         @endif
     </x-elements::block>
